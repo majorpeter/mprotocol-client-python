b1f60e5dde083960190f5e967e1e55b4439f4a61
Major Péter <majorpeter29@gmail.com>
2017-10-28 15:23:42 +0200

    added support for property results in ProtocolResult

--- ProtocolResult.py
+++ ProtocolResult.py
@@ -6,16 +6,28 @@ class ProtocolResult:
 
     def __init__(self, response_line, data=None):
         match = re.match('^E([0-9]):', response_line)
-        if not match:
-            raise ValueError('Invalid response line: %s' % response_line)
+        if match:
+            self.ordinal = int(match[1])
+            self.message = response_line.split(':')[1]
+            self.data = data
+        elif response_line.startswith('P_'):
+            self.ordinal = 0
+            self.message = 'Ok'
 
-        self.ordinal = int(match[1])
-        self.message = response_line.split(':')[1]
-        self.data = data
+            self.data = {
+                'type': response_line[2:response_line.index(' ')],
+                'value': response_line[response_line.index('=')+1:]
+            }
+        else:
+            raise ValueError('Invalid response line: %s' % response_line)
 
     @staticmethod
     def is_valid_result(response_line):
-        return re.match('^E([0-9]):', response_line)
+        if re.match('^E([0-9]):', response_line):
+            return True
+        if response_line.startswith('P_'):
+            return True
+        return False
 
     def __bool__(self):
         return self.ordinal == 0
