834b7e6a536a66407eb09c238043349b8eed5c6f
Major Péter <majorpeter29@gmail.com>
2017-10-28 17:30:08 +0200

    refactor

--- NodeProperty.py
+++ NodeProperty.py
@@ -1,16 +1,24 @@
 class NodeProperty:
+    actual_attributes = ['_client', '_path']
+
     def __init__(self, client, path=[]):
         self._client = client
         self._path = path
 
+    def get_path_as_node(self):
+        return '/' + ' / '.join(self._path)
+
+    def get_path_as_property(self):
+        return '/' + '/'.join(self._path[:-1]) + '.' + self._path[-1]
+
     def protocol_get_node(self):
-        return self._client.send_sync('GET /' + '/'.join(self._path))
+        return self._client.send_sync('GET ' + self.get_path_as_node())
 
     def protocol_get_property_value(self):
-        return self._client.send_sync('GET /' + '/'.join(self._path[:-1]) + '.' + self._path[-1])
+        return self._client.send_sync('GET ' + self.get_path_as_property())
 
     def protocol_call_method(self, argument):
-        cmd = 'CALL /' + '/'.join(self._path[:-1]) + '.' + self._path[-1]
+        cmd = 'CALL ' + self.get_path_as_property()
         if argument:
               cmd += '=' + argument
         result = self._client.send_sync(cmd)
@@ -20,7 +28,7 @@ class NodeProperty:
         return NodeProperty(self._client, self._path + [name])
 
     def __setattr__(self, key, value):
-        if key in ['_client', '_path']:
+        if key in NodeProperty.actual_attributes:
             object.__setattr__(self, key, value)
         else:
             result = self._client.send_sync('SET /' + '/'.join(self._path) + '.' + key + '=' + value)
