167970af0c51239c75b8cb689f47aa7bec147a1e
Major Péter <majorpeter29@gmail.com>
2017-10-28 17:24:30 +0200

    added support for CALL in NodeProperty

--- NodeProperty.py
+++ NodeProperty.py
@@ -9,6 +9,13 @@ class NodeProperty:
     def protocol_get_property_value(self):
         return self._client.send_sync('GET /' + '/'.join(self._path[:-1]) + '.' + self._path[-1])
 
+    def protocol_call_method(self, argument):
+        cmd = 'CALL /' + '/'.join(self._path[:-1]) + '.' + self._path[-1]
+        if argument:
+              cmd += '=' + argument
+        result = self._client.send_sync(cmd)
+        return result
+
     def __getattr__(self, name):
         return NodeProperty(self._client, self._path + [name])
 
@@ -25,6 +32,15 @@ class NodeProperty:
         if not result:
             raise BaseException('Could not get property value: ' + str(self._path))
         return result.data['value']
+
+    def __call__(self, *args, **kwargs):
+        if len(args) > 1:
+            raise BaseException('Too many args in method call: ' + str(self._path))
+
+        arg = ''
+        if len(args) == 1:
+            arg = str(args[0])
+        return self.protocol_call_method(arg)
 
     def __getitem__(self, item):
         return str(getattr(self, item))
